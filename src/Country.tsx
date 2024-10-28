c689aa03da61ad353a33505981f2bcd909194a1e
Leeyoungryul <lekjkj@naver.com>
2024-10-29 00:22:03 +0900

    10/29 atom 생성 및 값 전달 구현

--- src/Country.tsx
+++ src/Country.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import {useForm} from "react-hook-form";
+import {useRecoilState, useSetRecoilState} from "recoil";
+import {countryAtom} from "./atom";
 
 const Container = styled.div`
     display: flex;
@@ -14,17 +16,32 @@ const Form = styled.form`
     
 `;
 
+const List = styled.div``;
+
 
 interface IData {
     name: string;
+    errors: {
+        name: {message: string}
+    }
 }
 
+
 function Country() {
-    const { register, handleSubmit } = useForm<IData>();
+    const { register,watch,formState:{errors}, handleSubmit, setError, setValue } = useForm<IData>();
+    const [country, setCountryAtom] = useRecoilState(countryAtom);
 
     const onValid = (data:IData) => {
-        console.log("data",data)
+        if( data.name === "" ){
+            setError("name", {message: "내용을 입력하세요"})
+        }
+        else{
+            setCountryAtom(data.name)
+            setValue("name", "")
+        }
     }
+
+
 
     return (
         <Container>
@@ -33,7 +50,13 @@ function Country() {
                 <input {...register("name")}
                        placeholder="이름" />
                 <button>가자!</button>
-                <span></span>
+                <List>
+                    <span>{country}</span>
+                    <button>✅</button>
+                    <button>🗑</button>
+                </List>
+
+                <span>{errors?.name?.message}</span>
             </Form>
             <Form>
                 <h1>내가 가본 나라들</h1>
---
+++ src/atom.ts
@@ -0,0 +1,6 @@
+import {atom} from "recoil";
+
+export const countryAtom = atom({
+    key: "name",
+    default: ""
+});
